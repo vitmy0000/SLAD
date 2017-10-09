8bda4dd7cfa918ff039445241c59d98f9ea359e2
weiz <weiz@coe-biomac3002.med.buffalo.edu>
2017-10-09 10:43:48 -0400

    improve demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,54 +1,69 @@
 #!/usr/bin/env bash
 set -o errexit
 set -o pipefail
-set -o nounset
+# set -o nounset
 # set -o xtrace
+
+# SET VARIABLES
+INPUT_FASTA="./seqs.fa"
+OUTPUT_DIR="./res"
+MASTER_URL="spark://coe-biomac3002.med.buffalo.edu:7077"
+SPARK_DIR="../Spark"
+SLAD_DIR="../SLAD"
+VSEARCH_DIR="../vsearch"
+MEMORY=8G
+NUM_CORE=4
+OTU_LEVEL=0.97
+NUM_LEAVE_CLUSTER=8
+## For termination condition, we recommend to set 
+## NUM_LEAVE_CLUSTER to 2 X NUM_CORE
+## and leave other two as default.
+## Please check the paper for more detailed discussion.
+RADIUS=0.15
+MIN_SIZE=100
+WORD_SIZE=8
+ABUNDANCE=2
+NUM_POWER_ITERATION=10
+RANDOM_SEED=0
 
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 
-# Arguments
-if [ "$#" -ne 1 ]; then
-    echo "Please provide <MASTER_URL>"
-    exit
-fi
-MASTER_URL="${1}"
-
 # Top-level partition phase
-rm -rf res
-mkdir -p res
+rm -rf ${OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
 time \
-../Spark/bin/spark-submit \
+${SPARK_DIR}/bin/spark-submit \
 --master "${MASTER_URL}" \
---conf spark.default.parallelism=16 \
---conf spark.executor.memory=8G \
+--conf spark.default.parallelism=$((4*${NUM_CORE})) \
+--conf spark.executor.memory=${MEMORY} \
 --class "com.weiz.slad.Program" \
 --jars \
-../SLAD/lib/scallop_2.11-2.1.2.jar \
-../SLAD/target/scala-2.11/slad_2.11-0.1.0.jar \
---input-file-path "./seqs.fa" \
---output-dir "./res/" \
---word-size 8 \
---abundance 2 \
---radius 0.15 \
---min-size 100 \
---num-leave-cluster 8 \
---num-power-iteration 10 \
---random-seed 0 \
+${SLAD_DIR}/lib/scallop_2.11-2.1.2.jar \
+${SLAD_DIR}/target/scala-2.11/slad_2.11-0.1.0.jar \
+--input-file-path "${INPUT_FASTA}" \
+--output-dir "${OUTPUT_DIR}" \
+--word-size ${WORD_SIZE} \
+--abundance ${ABUNDANCE} \
+--radius ${RADIUS} \
+--min-size ${MIN_SIZE} \
+--num-leave-cluster ${NUM_LEAVE_CLUSTER} \
+--num-power-iteration ${NUM_POWER_ITERATION} \
+--random-seed ${RANDOM_SEED} \
 2> /dev/null 
-../vsearch/bin/vsearch -usearch_global "./seqs.fa" -db "./res/landmarks.fa" -id 0.6 -blast6out "./res/hit.txt" -strand plus -threads 4
+${VSEARCH_DIR}/bin/vsearch -usearch_global "${INPUT_FASTA}" -db "${OUTPUT_DIR}/landmarks.fa" -id 0.6 -blast6out "${OUTPUT_DIR}/hit.txt" -strand plus -threads ${NUM_CORE}
 mkdir res/clusters/
-python ../SLAD/scripts/partition.py -f "./seqs.fa" -u "./res/hit.txt" -o "./res/clusters" -c "./res/sub_count.txt"
+python ${SLAD_DIR}/scripts/partition.py -f "${INPUT_FASTA}" -u "${OUTPUT_DIR}/hit.txt" -o "${OUTPUT_DIR}/clusters" -c "${OUTPUT_DIR}/sub_count.txt"
 rm -r res/derep
 rm -r res/partition
 
 # Sub-clustering phase
-for x in $(ls ./res/clusters/); do
+for x in $(ls ${OUTPUT_DIR}/clusters/); do
     { \
-    ../vsearch/bin/vsearch --sortbylength ./res/clusters/${x} --output ./res/clusters/${x}_sorted.fa; \
-    ../vsearch/bin/vsearch -cluster_smallmem ./res/clusters/${x}_sorted.fa -id 0.97 -centroids ./res/clusters/${x}_centroids.fa -userout ./res/clusters/${x}_user.txt -userfields query+target+id; \
+    ../vsearch/bin/vsearch --sortbylength ${OUTPUT_DIR}/clusters/${x} --output ${OUTPUT_DIR}/clusters/${x}_sorted.fa; \
+    ../vsearch/bin/vsearch -cluster_smallmem ${OUTPUT_DIR}/clusters/${x}_sorted.fa -id ${OTU_LEVEL} -centroids ${OUTPUT_DIR}/clusters/${x}_centroids.fa -userout ${OUTPUT_DIR}/clusters/${x}_user.txt -userfields query+target+id; \
     rm ./res/clusters/${x}_sorted.fa; \
     } &
 done
